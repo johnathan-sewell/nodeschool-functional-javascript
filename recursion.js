abc343b053feb90f30aa20e1b61b7f400bf69ee1
Jonathan Sewell <jonathans@brandwatch.com>
2014-10-30 09:05:22 +0000

    refactored reduce function

--- recursion.js
+++ recursion.js
@@ -1,22 +1,8 @@
-function reduce(arr, fn, initial) {
-	return (function reduceOne(index, value) {
-			if (index > arr.length - 1) return value // end condition
-			return reduceOne(index + 1, fn(value, arr[index], index, arr)) // calculate & pass values to next step
-		})(0, initial) // IIFE. kick off recursion with initial values
-}
-
-//Mine
-var index = 0;
-function reduce(arr, func, memo) {
-	var current, 
-		tail;
-
-	if(arr.length === 0) return memo;
-
-	current = arr[0];
-	func(memo, current, index++, arr);
-
-	tail = arr.slice(1);
-	return reduce(tail, func, memo);
+function reduce(arr, func, initial) {
+	return (function reduceRecursive(memo, index) {
+		if (index > arr.length - 1) return memo;
+		func(memo, arr[index], index, arr);
+		return reduceRecursive(memo, ++index);
+	})(initial, 0);
 }
 module.exports = reduce;
